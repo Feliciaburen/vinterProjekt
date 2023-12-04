779a127d87badf69045e108734205255b4512584
felicia <felicia.buren@ga.ntig.se>
2023-12-04 10:53:56 +0100

    påbörjat spelet PacMan som vinterprojektet

--- Program.cs
+++ Program.cs
@@ -3,8 +3,24 @@ using Raylib_cs;
 
 class Program
 {
+    private Circle points = new Circle();
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+
+        Raylib.InitWindow(800, 600, "Pac-Man");
+        Raylib.SetTargetFPS(60);
+        
+        Texture2D PacMan = Raylib.LoadTexture(@"resurser/pacman.png");
+
+        Raylib.DrawTexture(PacMan, 40, 300, Color.WHITE);
+        
+        while (!Raylib.WindowShouldClose())
+        {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.BLACK);
+            
+            Raylib.EndDrawing();
+        }
     }
 }
